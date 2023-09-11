a7fed955adc316cbd2abe62660c436f3b87b1558
Nguyen Mai <mitluvs9@gmail.com>
2023-09-11 21:01:25 +0700

    setting router

---
+++ client/src/pages/Pagenotfound.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./../components/Layout/layout";
+
+const Pagenotfound = () => {
+  return (
+    <Layout>
+      <h1>ERROR 404</h1>
+    </Layout>
+  );
+};
+
+export default Pagenotfound;
