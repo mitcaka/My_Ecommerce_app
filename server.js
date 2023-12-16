52d39808ca210f7b44aa603b1c3869c955d4b14e
Nguyen Mai <mitluvs9@gmail.com>
2023-12-17 02:33:36 +0700

    deploy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
+import path from "path";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 
@@ -21,6 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -30,6 +32,10 @@ app.use("/api/v1/product", productRoutes);
 //rest api
 app.get("/", (req, res) => {
   res.send("<h1>Mit lam MERN stack</h1>");
+});
+
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 //Port
